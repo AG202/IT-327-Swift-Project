d1065f478e908c1ed0f8d6453c446b94582c372a
PTR09 <71910300+PTR09@users.noreply.github.com>
2023-12-05 18:19:07 -0600

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  IT327Codes
+//
+//  Created by Peter Lam on 12/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
